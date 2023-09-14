c51bac44369e7ce2ef3cd8419be7bfd0963e21fb
Yui San ae2f <84163330+YuiSanae2f@users.noreply.github.com>
2023-09-14 18:07:00 +0900

    .

---
+++ docs/Request.js
@@ -0,0 +1,15 @@
+strArr = [
+    {id: "translate", ko: "언어", en: "Languages"},
+
+    {
+        id: "summary[0]", 
+        ko: "본 클래스의 목적은 분류형 모델을 학습시킬 시에 학습시킬 정보를 정리하는 것입니다.",
+        en: "This class is to organise the data to educate the classifier AI model."
+    },
+
+    {
+        id: "summary[1]",
+        ko: '본 클래스는 <a href="#input" class="mark">입력</a>과 <a href="#output" class="mark">기대 출력</a>의 쌍으로 이루어져 있습니다.',
+        en: 'This class is composed of the pair of <a href="#input" class="mark">the input format</a> and <a href="#output" class="mark">the output format expected</a>.'
+    }
+];
--- docs/global.js
+++ docs/global.js
@@ -16,6 +16,21 @@ function endl(count = 1) {
 
 document.write('<p class="indent" style="color:rgb(95, 95, 95); font-size: 10px;">© 2023. YuiSanae2f</p>');
 document.getElementById("_t").innerHTML = `<h2 id="translate"></h2><div class="box indent"><d onclick="_translate('ko')">한국어</d><br><d onclick="_translate('en')">English</d><br></div>`;
-_translate("ko");
+
+// 현재 페이지의 URL에서 쿼리 매개변수 가져오기
+const queryString = window.location.search;
+
+// URLSearchParams 객체를 사용하여 쿼리 매개변수 파싱
+const urlParams = new URLSearchParams(queryString);
+
+// "q" 매개변수의 값을 가져오기
+const langCode = urlParams.get('lang');
+if(langCode == null) langCode = "ko";
+
+_translate(langCode);
+aEl = document.getElementsByTagName('a');
+for(let i = 0; i < aEl.length; i++) {
+    aEl[i].href += `?lang=${langCode}`;
+}
 
 // © 2023. YuiSanae2f
