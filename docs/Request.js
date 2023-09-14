ef9ab0f673c9bfac3fc147f3a58c55b32de4b900
Yui San ae2f <84163330+YuiSanae2f@users.noreply.github.com>
2023-09-14 20:54:53 +0900

    .

--- docs/Request.js
+++ docs/Request.js
@@ -11,5 +11,52 @@ strArr = [
         id: "summary[1]",
         ko: '본 클래스는 <a href="#input" class="mark">입력</a>과 <a href="#output" class="mark">기대 출력</a>의 쌍으로 이루어져 있습니다.',
         en: 'This class is composed of the pair of <a href="#input" class="mark">the input format</a> and <a href="#output" class="mark">the output format expected</a>.'
+    },
+
+    {
+        id: "summary[2]",
+        ko: '따라서 이는 동시에 예측을 수행할 시의 요청값으로 사용될 수 있습니다.',
+        en: 'It means it could also be used to request for the prediction for the model.'
+    },
+
+    {
+        id: "summary[3]",
+        ko: 
+        '모델은\n'
+        + '<a href="https://yuisanae2f.github.io/YuiSanae2f/dictionary/pointer/array/string" class="reservedKey">string</a>\n'
+        + '형태로 <a href="#input" class="mark">입력</a>을 받아\n'
+        + '<a href="#T" class="generic">T</a>의 형태의 <a href="#output" class="mark">출력값</a>을 예측할 것입니다.',
+
+        en: 
+        'This model will perform the prediction from <a href="#input" class="mark">the input</a> of\n'
+        + '<a href="https://yuisanae2f.github.io/YuiSanae2f/dictionary/pointer/array/string" class="reservedKey">string</a>\n'
+        + 'to <a href="#output" class="mark">the output</a> of <a href="#T" class="generic">T</a>.'
+    },
+
+    {
+        id: "T[0]",
+        ko: 
+        '<a href="#output" class="mark">기대 출력</a>의\n'
+        + '<a href="https://yuisanae2f.github.io/YuiSanae2f/dictionary/dataType">자료형</a>을 결정합니다.',
+        en: 
+        'It will determine\n'
+        + 'the <a href="https://yuisanae2f.github.io/YuiSanae2f/dictionary/dataType">data type</a>\n'
+        + 'of the <a href="#output" class="mark">expected output</a>'
+    },
+
+    {
+        id: "input[0]",
+        ko:
+        '본 모델은 기본적으로\n'
+        + '<a href="https://yuisanae2f.github.io/YuiSanae2f/dictionary/pointer/array/string" class="reservedKey">string</a>\n'
+        + '형태로 입력받는 것을 권장합니다.',
+        en:
+        ''
+    },
+
+    {
+        id: "output[0]",
+        ko: '출력값의 형태는 다양하게 표현될 수 있을 것입니다.',
+        en: ''
     }
 ];
--- docs/global.js
+++ docs/global.js
@@ -8,23 +8,17 @@ function _translate(code) {
 
     const aEl = document.getElementsByTagName('a');
 
-    for (let i = 0; i < aEl.length; i++) {
-        const currentURL = aEl[i].href;
-        const queryStringIndex = currentURL.indexOf('?'); // 쿼리 문자열 시작 위치 찾기
-
-        if (queryStringIndex !== -1) {
-            const baseURL = currentURL.substring(0, queryStringIndex); // 쿼리 문자열 제외한 부분 추출
-            aEl[i].href = `${baseURL}?lang=${langCode}`;
-
-        } else {
-            // 쿼리 문자열이 없는 경우, ?lang= 매개변수를 추가합니다.
-            aEl[i].href += `?lang=${langCode}`;
-        }
-    }
-
     document.getElementsByTagName("html")[0].lang = code;
     for (let i = 0; i < strArr.length; i++) {
         document.getElementById(strArr[i].id).innerHTML = strArr[i][code];
+    }
+
+    for (let i = 0; i < aEl.length; i++) {
+        const currentURL = aEl[i].href;
+        const arr = currentURL.split('#'); // 쿼리 문자열 시작 위치 찾
+        const pro0 = arr[0];
+        url = pro0.split('?')[0] + `?lang=${code}${arr[1] === undefined ? '' : '#' + arr[1]}`;
+        aEl[i].href = url;
     }
 }
 
@@ -35,20 +29,19 @@ function endl(count = 1) {
 // var langCode;
 
 function _init() {
-    // 현재 페이지의 URL에서 쿼리 매개변수 가져오기
-    const queryString = window.location.search;
-
-    // URLSearchParams 객체를 사용하여 쿼리 매개변수 파싱
-    const urlParams = new URLSearchParams(queryString);
+    // 주어진 URL 문자열
+    const urlString = window.location.href.toString();
 
     // "q" 매개변수의 값을 가져오기
-    langCode = urlParams.get('lang');
-    if(langCode === null) langCode = "ko";
+    let langCode0 = urlString.split('?')[1];
+    let langCode1 = langCode0 === undefined ? "ko" : langCode0.split('=')[1];
+    let langCode = langCode1 === undefined ? "ko" : langCode1.split('#')[0];
+
+    langCode = langCode === null ? "ko" : langCode;
 
     document.write('<p class="indent" style="color:rgb(95, 95, 95); font-size: 10px;">© 2023. YuiSanae2f</p>');
     document.getElementById("_t").innerHTML = `<h2 id="translate"></h2><div class="box indent"><d onclick="_translate('ko')">한국어</d><br><d onclick="_translate('en')">English</d><br></div>`;
 
-    document.write(langCode);
     _translate(langCode);
 }
 
